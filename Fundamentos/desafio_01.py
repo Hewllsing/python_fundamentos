c11381bce47598f3f2cecf581d25da5d987f92d4
Formando <formando@iMac-de-Administrador-3.local>
2025-11-28 09:48:14 +0000

    novos estudos, e primeiro desafio adicionado com random

---
+++ Fundamentos/desafio_01.py
@@ -0,0 +1,17 @@
+# Colocar o computador a gerar um numero aleatorio entre 1 e 6
+import random
+
+
+numero_aleatorio = random.randint(1, 6)
+qtd_tentativa = 0
+
+while qtd_tentativa < 3:
+    adivinhe_numero = input("Tente advinhar o numero entre 1 e 6: ")
+
+    if adivinhe_numero == numero_aleatorio:
+        print(f"O seu numero selecionado foi {adivinhe_numero} e o numero sorteado {numero_aleatorio}, parabens voce acertou!")
+        break
+    else:
+        print(f"Não é {adivinhe_numero}. Tente novamente!")
+        qtd_tentativa += 1
+else: print(f"O numero sorteado foi {numero_aleatorio}. Mais sorte na proxima...")
