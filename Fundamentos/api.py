42c493dee99015c02db7fd8982f0b722a144ea65
Formando <formando@iMac-de-Administrador-3.local>
2025-12-05 16:44:05 +0000

    adicionando api com cotacoes para converter em real

---
+++ Fundamentos/api.py
@@ -0,0 +1,11 @@
+import requests
+
+# Faz a requisição via link da API
+cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+# Atribui os valores em formato json para dicionario
+cotacoes = cotacoes.json()
+print(cotacoes)
+
+# Para buscar o valor da cotacao de dolar para real
+cotacao_dolar = cotacoes["USDBRL"]["bid"]
+print(cotacao_dolar)
